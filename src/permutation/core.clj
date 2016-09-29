5e93a21f536763059b72443828bf02e532fba96b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-09-29 09:42:13 +0200

    Create a multiply function

--- src/permutation/core.clj
+++ src/permutation/core.clj
@@ -1,6 +1,8 @@
 (ns permutation.core)
 
-(defn swap
-  "I swap my arguments"
-  [x y]
-  [y x])
+(defn multiply
+  "multiply two permutations to produce a third"
+  [g h]
+  (let [tuple (fn [x] [x (g (h x))])
+        combine (fn [acc t] (assoc acc (t 0) (t 1)))]
+    (reduce combine {} (map tuple (keys g)))))
--- test/permutation/core_test.clj
+++ test/permutation/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [permutation.core :refer :all]))
 
-(deftest swap-test
-  (testing "should swap her arguments"
-    (is (= (swap 0 1) [1 0]))))
+(deftest multiply-test
+  (testing "should multiply transpositions"
+    (is (= (multiply {0 0, 1 2, 2 1} {0 1, 1 0, 2, 2}) {0 2, 1, 0, 2 1}))))
