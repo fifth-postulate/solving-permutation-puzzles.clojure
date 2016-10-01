f523b2a67120019fc9889e723d133ad9cc0293aa
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-10-01 06:38:53 +0200

    Abstract into a create-permutation function

--- src/permutation/core.clj
+++ src/permutation/core.clj
@@ -1,22 +1,22 @@
 (ns permutation.core)
 
+(defn- create-permutation
+  "create a permutation by forming tuples over a G-set"
+  [tuple gset]
+  (let [combine (fn [acc t] (assoc acc (t 0) (t 1)))]
+    (reduce combine {} (map tuple gset))))
+
 (defn multiply
   "multiply two permutations to produce a third"
   [g h]
-  (let [tuple (fn [x] [x (g (h x))])
-        combine (fn [acc t] (assoc acc (t 0) (t 1)))]
-    (reduce combine {} (map tuple (keys g)))))
+  (create-permutation (fn [x] [x (g (h x))]) (keys g)))
 
 (defn inverse
   "determine the inverse of a permutation"
   [g]
-  (let [tuple (fn [x] [(g x) x])
-        combine (fn [acc t] (assoc acc (t 0) (t 1)))]
-    (reduce combine {} (map tuple (keys g)))))
+  (create-permutation (fn [x] [(g x) x]) (keys g)))
 
 (defn identity-for
   "return the identity permutation on the same G-set"
   [g]
-  (let [tuple (fn [x] [x x])
-        combine (fn [acc t] (assoc acc (t 0) (t 1)))]
-    (reduce combine {} (map tuple (keys g)))))
+  (create-permutation (fn [x] [x x]) (keys g)))
