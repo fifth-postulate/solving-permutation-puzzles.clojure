ce99cb42fd1495ebeec4ef6eb84e3a6f6e4941e1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-11-29 21:14:37 +0100

    check generators for next level

--- test/permutation/bsgs_test.clj
+++ test/permutation/bsgs_test.clj
@@ -4,10 +4,12 @@
 
 (deftest level-test
   (testing "creation of level"
-    (let [[l _] (level [{0 1, 1 0}])]
-      (is (= l {:base 0, :generators [{0 1, 1 0}], :orbit [0 1], :transversal [{0 0, 1 1} {0 1, 1 0}]})))
-    (let [[l _] (level [{0 1, 1 0, 2 2}, {0 0, 1 2, 2 1}])]
-      (is (= l {:base 0, :generators [{0 1, 1 0, 2 2} {0 0, 1 2, 2 1}], :orbit [0 1 2], :transversal [{0 0, 1 1, 2 2} {0 1, 1 0, 2 2} {0 2, 1 0, 2 1}]})))))
+    (let [[l gs] (level [{0 1, 1 0}])]
+      (is (= l {:base 0, :generators [{0 1, 1 0}], :orbit [0 1], :transversal [{0 0, 1 1} {0 1, 1 0}]}))
+      (is (= gs [])))
+    (let [[l gs] (level [{0 1, 1 0, 2 2}, {0 0, 1 2, 2 1}])]
+      (is (= l {:base 0, :generators [{0 1, 1 0, 2 2} {0 0, 1 2, 2 1}], :orbit [0 1 2], :transversal [{0 0, 1 1, 2 2} {0 1, 1 0, 2 2} {0 2, 1 0, 2 1}]}))
+      (is (= gs [{2 1, 0 0, 1 2} {0 0, 1 2, 2 1}])))))
 
 (deftest sift-test
   (testing "should sift elements"
