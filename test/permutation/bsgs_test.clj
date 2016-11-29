b3a9a4704db2d19527eef5abe16f24ae312e7484
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-11-29 20:49:25 +0100

    Change order of let and is

--- test/permutation/bsgs_test.clj
+++ test/permutation/bsgs_test.clj
@@ -4,10 +4,10 @@
 
 (deftest level-test
   (testing "creation of level"
-    (is (let [[l _] (level [{0 1, 1 0}])]
-          (= l {:base 0, :generators [{0 1, 1 0}], :orbit [0 1], :transversal [{0 0, 1 1} {0 1, 1 0}]})))
-    (is (let [[l _] (level [{0 1, 1 0, 2 2}, {0 0, 1 2, 2 1}])]
-          (= l {:base 0, :generators [{0 1, 1 0, 2 2} {0 0, 1 2, 2 1}], :orbit [0 1 2], :transversal [{0 0, 1 1, 2 2} {0 1, 1 0, 2 2} {0 2, 1 0, 2 1}]})))))
+    (let [[l _] (level [{0 1, 1 0}])]
+      (is (= l {:base 0, :generators [{0 1, 1 0}], :orbit [0 1], :transversal [{0 0, 1 1} {0 1, 1 0}]})))
+    (let [[l _] (level [{0 1, 1 0, 2 2}, {0 0, 1 2, 2 1}])]
+      (is (= l {:base 0, :generators [{0 1, 1 0, 2 2} {0 0, 1 2, 2 1}], :orbit [0 1 2], :transversal [{0 0, 1 1, 2 2} {0 1, 1 0, 2 2} {0 2, 1 0, 2 1}]})))))
 
 (deftest sift-test
   (testing "should sift elements"
