5be8ef4ddc2f7ea86c718c98b85c6a19800a201b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-12-01 00:56:57 +0100

    Evaluate expressions

--- src/permutation/expression.cljc
+++ src/permutation/expression.cljc
@@ -14,3 +14,13 @@
   "returns an expression for the nth generator"
   [n]
   {:type :generator :index n})
+
+(defn evaluate
+  "evaluates an expression in terms of a multiplier inverter and generator"
+  [multiplier inverter generator expression]
+  (case (:type expression)
+    :generator (generator (:index expression))
+    :inverse (inverter (evaluate multiplier inverter generator (:expression expression)))
+    :multiply (multiplier
+               (evaluate multiplier inverter generator (:left expression))
+               (evaluate multiplier inverter generator (:right expression)))))
--- test/permutation/expression_test.clj
+++ test/permutation/expression_test.clj
@@ -1,5 +1,6 @@
 (ns permutation.expression-test
-  (:require [permutation.expression :refer :all]
+  (:require [permutation.kernel :as kernel]
+            [permutation.expression :refer :all]
             [clojure.test :refer :all]))
 
 (deftest expression-test
@@ -12,3 +13,19 @@
       (is (= (multiply g h) {:type :multiply
                              :left {:type :generator :index 0}
                              :right {:type :generator :index 1}})))))
+
+(deftest evaluation-test
+  (testing "should correctly evaluate an expression"
+    (let [g (generator 0)
+          h (generator 1)
+          t {0 1, 1 2, 2 0}
+          s {0 1, 1 0, 2 2}
+          generators [t s]
+          gens #(nth generators %)
+          inv #(kernel/inverse %)
+          mult #(kernel/multiply %1 %2)
+          ev (partial evaluate mult inv gens)]
+      (is (= (ev g) t))
+      (is (= (ev h) s))
+      (is (= (ev (inverse g)) (kernel/inverse t)))
+      (is (= (ev (multiply (inverse g) h)) (kernel/multiply (kernel/inverse t) s))))))
